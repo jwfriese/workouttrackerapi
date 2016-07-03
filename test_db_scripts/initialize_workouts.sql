bd33bcc04dc0a4fc9b867094d078dd20ad40bcd9
Jared Friese <jared.friese@gmail.com>
2016-07-03 06:42:28 -0700

    Use lift IDs instead of names in workout table
    [#124382141]

--- test_db_scripts/initialize_workouts.sql
+++ test_db_scripts/initialize_workouts.sql
@@ -4,12 +4,12 @@ CREATE TABLE workouts(
         id serial PRIMARY KEY,
         name varchar(256),
         timestamp timestamptz,
-        lifts varchar(128)[]
+        lifts integer[]
 ); 
 
-INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle one','2016-03-07 06:26:34.0-0800','{turtle lift,turtle press,turtle push}');
-INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle two','2016-03-09 06:04:44.0-0800','{turtle press,turtle cleans}');
-INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle three','2016-03-11 05:57:56-0800','{turtle lift,turtle press,turtle push}');
-INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle four','2016-03-15 05:57:00-0700','{turtle cleans,turtle lift,turtle box jumps}');
-INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle five','2016-03-16 05:46:51-0700','{turtle hangs}');
+INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle one','2016-03-07 06:26:34.0-0800','{1,2,3}');
+INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle two','2016-03-09 06:04:44.0-0800','{4,5}');
+INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle three','2016-03-11 05:57:56-0800','{6,7,8}');
+INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle four','2016-03-15 05:57:00-0700','{9,10,11}');
+INSERT INTO workouts (name,timestamp,lifts) VALUES ('turtle five','2016-03-16 05:46:51-0700','{12}');
 
