0005d5a49f8db8675f488743f4f4ed37eeb0b28e
Jared Friese <jared.friese@gmail.com>
2016-08-19 06:46:31 -0700

    Write id, dataTemplate, and lift fields to JSON for a set model
    [#126938107]

--- lifts/sets/datamodel/set.go
+++ lifts/sets/datamodel/set.go
@@ -1,9 +1,9 @@
 package datamodel
 
 type Set struct {
-	Id            int      `json:"-"`
-	DataTemplate  string   `json:"-"`
-	Lift          int      `json:"-"`
+	Id            int      `json:"id"`
+	DataTemplate  string   `json:"dataTemplate"`
+	Lift          int      `json:"lift"`
 	Weight        *float32 `json:"weight,omitempty"`
 	Height        *float32 `json:"height,omitempty"`
 	TimeInSeconds *float32 `json:"timeInSeconds,omitempty"`
